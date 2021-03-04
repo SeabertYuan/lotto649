8a1146c5e913e3e1115f6f9b6cadd3b99565ffee
Seabert Yuan <seabert@pop-os.localdomain>
2021-03-03 22:23:15 -0700

    first push

---
+++ lotto649.py
@@ -0,0 +1,198 @@
+import pygame
+import pygame_gui
+import lotto649driver as ld
+import time
+
+#0 Start by asking whether or not they want to generate their numbers
+#1 If yes, move to step 3
+#2 If no, ask them 6 times for a number
+#3 Confirm their numbers, if yes, say locked in and move to step 5
+#4 Change numbers based on which index they give.
+#5 Generate 6 random numbers as the winning ticket
+#5 One by one show them and way whether or not they won
+#5 Show odds of winning for every win
+#6 Ask if they want to play again and either do nothing (no) or restart (yes)
+
+pygame.init() #Initialize pygame
+backend = ld.lottologic() # make a new object
+
+pygame.display.set_caption('Lotto 649') #Set title of window to 'Lotto 649'
+window_surface = pygame.display.set_mode((800, 600)) #Set window to 800px by 600px (x,y)
+
+#Sets the background to the window size and sets the colour to black
+background = pygame.Surface((800, 600))
+background.fill(pygame.Color('#000000'))
+
+manager = pygame_gui.UIManager((800, 600)) #manages every element on the window
+
+text = '<b>Welcome to lotto 649!<br>Would you like to generate random numbers?</b>' #initialize initial text
+selected_number = '<b></b>' #initialize empty ticket
+
+#Box that contains text to be displayed
+information_box = pygame_gui.elements.UITextBox(text,
+                                                    relative_rect=pygame.Rect((250,50),(300,150)),
+                                                    manager=manager)
+information_box.set_active_effect(pygame_gui.TEXT_EFFECT_TYPING_APPEAR)
+#Box that displays your ticket number
+number_box = pygame_gui.elements.UITextBox(selected_number,
+                                           relative_rect=pygame.Rect((275,200),(250,50)),
+                                           manager=manager)
+#Button that is for yes
+yes_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((275, 375), (100, 50)), text='Yes',
+                                                    manager=manager)
+#Button that is for no
+no_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((425, 375), (100, 50)),
+                                         text='No',
+                                         manager=manager)
+#Button at the very end to prompt the user for results
+result_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((325, 325), (150,35)),
+                                             text='Press for result',
+                                             manager=manager)
+result_button.hide() #hide the button by default
+
+### Updates a boxes text
+def change_information(text, information_box):
+#Updates the box
+    information_box = pygame_gui.elements.UITextBox(text,
+                                                    relative_rect=pygame.Rect((250,50),(300,150)),
+                                                    manager=manager)
+    information_box.set_active_effect(pygame_gui.TEXT_EFFECT_TYPING_APPEAR)
+#Field for entering number
+number_entry = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((350, 275), (100,150)),
+                                                   manager=manager)
+def change_number_display(selected_number, number_box):
+#Box that displays your ticket number
+    number_box = pygame_gui.elements.UITextBox(selected_number,
+                                               relative_rect=pygame.Rect((275,200),(250,50)),
+                                               manager=manager)
+
+clock = pygame.time.Clock() #used by pygame_gui to look for input updates
+is_running = True
+stage = 0 # stages 0-6 0 is start stage, all the way to reset stage (see above)
+i = 1 #variable for looking through ticket_numbers, and for changing ticket_numbers
+
+#function to call and change the textbox used in stage 5
+def compare(text, information_box):
+    text = backend.winningNums(text)
+    change_information(text, information_box)
+
+#While pygame is running
+while is_running:
+    time_delta = clock.tick(60)/1000.0
+    number_entry.set_allowed_characters('numbers')
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            is_running = False
+
+        manager.process_events(event)
+        if event.type == pygame.USEREVENT:
+            if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                if event.ui_element == yes_button:
+                    print('Yes button pressed.')
+                    if stage == 0: #if yes pressed when asked to generate numbers
+                        backend.yn_button = 1
+                        backend.generated_num()
+                        stage=2
+                        text = backend.confirm_numbers(text)
+                        change_information(text, information_box)
+                        for num in backend.ticket_numbers:
+                            if selected_number.endswith('</b>'):
+                                selected_number = selected_number[:-4]+str(num)+' </b>'
+                        change_number_display(selected_number, number_box)
+                        backend.yn_button = 0
+                    elif stage == 2: #if yes pressed when asked to confirm
+                        stage = 5
+                        backend.yn_button = 1
+                        text = backend.change_numbers(text, backend.yn_button)
+                        change_information(text, information_box)
+                        no_button.disable()
+                        yes_button.disable()
+                        backend.yn_button = 0
+                        compare(text, information_box)
+                        result_button.show()
+                    elif stage == 6:
+                        stage = 0
+                        selected_number = '<b></b>'
+                        change_number_display(selected_number, number_box)
+                        text = '<b>Welcome to lotto 649!<br>Would you like to generate random numbers?</b>'
+                        change_information(text, information_box)
+                        backend = ld.lottologic()
+                        
+                if event.ui_element == no_button:
+                    print('No button pressed.')
+                    if stage == 0: #if no pressed when asked to generate numbers
+                        backend.yn_button = 2
+                        stage=1
+                        change_information(backend.choose_number(text), information_box)
+                    elif stage == 2: #if no pressed when asked to confirm
+                        stage = 3
+                        backend.yn_button = 2
+                        change_information(backend.change_numbers(text, backend.yn_button), information_box)
+
+                if event.ui_element == result_button:
+                    print('Result button pressed.')
+                    if stage == 5:
+                        text = backend.comparison(text)[:-4]+'<br>Retry? (Yes/No)</b>'
+                        change_information(text, information_box)
+                        result_button.hide()
+                        yes_button.enable()
+                        no_button.enable()
+                        stage = 6
+            if event.user_type == pygame_gui.UI_TEXT_ENTRY_CHANGED:
+                if event.ui_element == number_entry:
+                    if stage == 1 and backend.yn_button == 2: #if selecting numbers and no was pressed
+                        write_string = str(event.text)
+                        if selected_number.endswith('</b>'):
+                            selected_number = selected_number[:-4] + write_string
+                        selected_number += '</b>'
+                        write_string = ''
+                        change_number_display(selected_number, number_box)
+                        selected_number = '<b></b>'
+                        for number in backend.ticket_numbers:
+                            selected_number = selected_number[:-4]+str(number)+' ' + '</b>'
+
+            if event.user_type  == pygame_gui.UI_TEXT_ENTRY_FINISHED:
+                if stage == 1:
+                    number_entry.set_text('')
+                    if i in range(1,7):
+                        promise = backend.choose_one_number(backend.ticket_numbers, event.text)
+                        if promise in range(1,50):
+                            backend.ticket_numbers.append(int(promise))
+                            if selected_number.endswith('</b>'):
+                                selected_number = selected_number[:-4]+str(promise)+' '
+                        else:
+                            text = promise
+                            change_information(text, information_box)
+                        i+=1
+                    else:
+                        stage=2
+                        text = backend.confirm_numbers(text)
+                        change_information(text, information_box)
+                    change_number_display(selected_number, number_box)
+                elif stage == 3: #called when no is pressed when asked to confirm numbers
+                    i = int(event.text)
+                    text = backend.select_change(event.text, text)
+                    change_information(text, information_box)
+                    number_entry.set_text('')
+                    stage = 4
+                elif stage == 4:
+                    promise = backend.choose_one_number(backend.ticket_numbers, event.text)
+                    if promise in range(1,50):
+                        backend.ticket_numbers[i-1] = promise
+                    number_entry.set_text('')
+                    selected_number = '<b>'
+                    for num in backend.ticket_numbers:
+                        selected_number += str(num)+' '
+                    selected_number += '</b>'
+                    change_number_display(selected_number, number_box) #Updates ticket display
+                    text = backend.confirm_numbers(text)
+                    change_information(text, information_box)
+                    stage = 2
+
+    manager.update(time_delta)
+    information_box.update(time_delta)
+
+    window_surface.blit(background, (0, 0))
+    manager.draw_ui(window_surface)
+
+    pygame.display.update()
---
+++ lotto649driver.py
@@ -0,0 +1,97 @@
+import random
+import math
+
+### class that contains all functions and methods to run lottery
+class lottologic:
+    #initializes constructors
+    def __init__(self):
+        self.ticket_numbers = [] #chosen or generated numbers
+        self.winning_numbers = [] #list for correct number set
+        self.yn_button = 0 #0 for null, 1 for yes, 2 for no
+    
+### confirm the numbers on the ticket
+    def confirm_numbers(self, text):
+      text = '<b>Your numbers are:<br></b>'
+      for num in self.ticket_numbers:
+        if text.endswith('</b>'):
+            text = text[:-4]
+        text += str(num) + ' '
+      text += '<br>Confirm? (Yes/No)</b>'
+      return text
+    def change_numbers(self,text, yn_button):
+        text = '<b></b>'
+        if yn_button == 2:
+            if text.endswith('</b>'):
+                text = text[:-4]
+            text += '<br>Which number would you like to change? (1-6)</b>'
+            return text
+        elif yn_button == 1:
+            text = '<b>Numbers locked in!</b>'
+            return text
+        return text
+    def select_change(self, number, text): #number is the user input
+        if int(number) > 6 or int(number) < 1:
+            text = '<b>Number out of range, keep number in range (1-6).</b>'
+            return text
+        text = '<b>Enter a number, then press enter.</b>'
+        return text
+
+### gets one valid input at a time
+    def choose_one_number(self, ticket_numbers, input_number):
+        if int(input_number) > 49:
+            text = '<b>Number is too lage!</b>'
+            return text
+        if int(input_number) in ticket_numbers:
+            text = '<b>Number already in list</b>'
+            return text
+        return int(input_number)
+
+### user choosing numbers
+    def choose_number(self, text):
+        text = '<b>Enter a number and press enter.</b>' #sets text on information box
+        return text
+
+###  random generated numbers
+    def generated_num(self):
+      for i in range(1, 7):
+        random_num = random.randint(1, 49)
+        while random_num in self.ticket_numbers: #will keep generating random numbers until number is distinct
+          random_num = random.randint(1, 49)
+        self.ticket_numbers.append(random_num)
+
+### Selects 6 random distinct numbers as the winning lottery numbers
+    def winningNums(self, text):
+        for i in range(6):
+            newNum = random.randint(1,49)
+            while newNum in self.winning_numbers:
+                newNum = random.randint(1,49)
+            self.winning_numbers.append(newNum)
+        if text.endswith('</b>'):
+            text = text[:-4]+'<br>Drawing numbers...'+'</b>'
+        return text
+
+    def comparison(self, text):
+        sameCounter = 0
+        text = '<b>Winning numbers are:<br></b>'
+        for j in self.winning_numbers:
+            if text.endswith('</b>'):
+                text = text[:-4]+str(j)+' </b>'
+        text = text[:-4]+'<br></b>'
+        for i in self.ticket_numbers:
+            if i in self.winning_numbers:
+                if text.endswith('</b>'):
+                    text = text[:-4]+'You won with: '+str(i)+'<br></b>'
+                print("You won with: " + str(i))
+                sameCounter+=1
+        text = text[:-4]+'<br>Probability of: '+self.probability(sameCounter)+'</b>'
+        return text
+
+    def probability(self, numberWon): #calculates the probability of your winnings
+        odds = 1.0
+        best_case = math.comb(49,6)
+        if numberWon == 6:
+            return str(best_case)
+        else:
+            winning_ticket = math.comb(6,numberWon)
+            non_winning = math.comb(43,(6-numberWon))
+            odds = (winning_ticket*non_winning)/best_case return str(odds)
